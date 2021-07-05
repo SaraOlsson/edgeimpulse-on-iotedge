bf2c69dbc87b0ef7a7aceec8c74ec86b81e7423e
Sara Olsson <sara.olsson@gaia.se>
2021-07-06 00:07:18 +0200

    safe up with test image if no camera device

--- modules/eicameradetect/main.py
+++ modules/eicameradetect/main.py
@@ -17,6 +17,10 @@ from azure.iot.device.aio import IoTHubModuleClient
 runner = None
 show_camera = False
 
+# global counters
+SCORE_THRESHOLD = 0.5
+TWIN_CALLBACKS = 0
+
 def now():
     return round(time.time() * 1000)
 
@@ -47,6 +51,8 @@ signal.signal(signal.SIGINT, sigint_handler)
 def help():
     print('python classify.py <path_to_model.eim> <Camera port ID, only required when more than 1 camera is present>')
 
+# if classifying image not from video
+# from here: https://github.com/edgeimpulse/linux-sdk-python/blob/master/edge_impulse_linux/image.py
 def get_features(ei_runner, image):
 
     features = []
@@ -89,11 +95,55 @@ def get_features(ei_runner, image):
             r = pixels[ix + 2]
             features.append((r << 16) + (g << 8) + b)
 
-    return features
+    return features, cropped
+
+def handle_inference_result(res, img, labels):
+
+    dict_result = {}
+
+    if "classification" in res["result"].keys():
+        print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
+        for label in labels:
+            score = res['result']['classification'][label]
+            print('%s: %.2f\t' % (label, score), end='')
+
+            dict_result = {
+                "class": label,
+                "score": score
+            }
+
+        print('', flush=True)
+
+        if (show_camera):
+            cv2.imshow('edgeimpulse', img)
+
+    elif "bounding_boxes" in res["result"].keys():
+        print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+        for bb in res["result"]["bounding_boxes"]:
+            response = '\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height'])
+            print(response)
+
+            # a Python object (dict):
+            dict_result = {
+                "class": bb['label'],
+                "score": bb['value'],
+                "rect": {
+                    "x": bb['x'],
+                    "y": bb['y'],
+                    "width": bb['width'],
+                    "height": bb['height']
+                }
+            }
+    
+    return dict_result
+
+
+async def send_json_telemetry(module_client, json_obj):
+    json_str = json.dumps(json_obj)
+    await module_client.send_message_to_output(json_str, "classification")
+
 
 async def main(argv):
-
-    #global module_client
 
     try:
         opts, args = getopt.getopt(argv, "h", ["--help"])
@@ -117,34 +167,53 @@ async def main(argv):
 
     print('MODEL: ' + modelfile)
 
-    # for IoT Edge
-    # The client object is used to interact with your Azure IoT hub.
+    # connect IoT Edge client
+    # the client object is used to interact with your Azure IoT hub.
     module_client = IoTHubModuleClient.create_from_edge_environment()
-    # connect the client.
     await module_client.connect()
+    #print('Run listeners')
+    #listeners = await asyncio.gather(twin_patch_listener(module_client))
+    #asyncio.run(listeners)
 
+    # use ML model and read camerastream
     with ImageImpulseRunner(modelfile) as runner:
         try:
             model_info = runner.init()
             print('Loaded runner for "' + model_info['project']['owner'] + ' / ' + model_info['project']['name'] + '"')
             labels = model_info['model_parameters']['labels']
             
-            # note to self: put single image test here
-
+            # define device id with argument or search device
+            found_camera = False
             if len(args)>= 2:
                 videoCaptureDeviceId = int(args[1])
+                found_camera = True
             else:
                 port_ids = get_webcams()
                 if len(port_ids) == 0:
                     print('Cannot find any webcams')
-                    return
-                    #raise Exception('Cannot find any webcams')
-                if len(args)<= 1 and len(port_ids)> 1:
+                elif len(args)<= 1 and len(port_ids)> 1:
                     print('Multiple cameras found. Add the camera port ID as a second argument to use to this script')
-                    return
-                    #raise Exception("Multiple cameras found. Add the camera port ID as a second argument to use to this script")
-                videoCaptureDeviceId = int(port_ids[0])
+                else:
+                    print('trying with videoCaptureDeviceId anyway?')
+                    videoCaptureDeviceId = int(port_ids[0])
+                    found_camera = True
 
+            # if no camera, try test image then exit
+            if(found_camera == False):
+                print("Did not found camera. Use test image")
+                image_path = os.path.join(dir_path, "model_test.jpg")
+                test_image = cv2.imread(image_path)
+                features, cropped = get_features(runner, test_image)
+                res = runner.classify(features)
+                handled_result = handle_inference_result(res, cropped, labels)
+                await send_json_telemetry(module_client, handled_result)
+                
+
+                print('classification runner response:', res, 'Sleep 5 sec then exit')
+                time.sleep(5)
+                return
+        
+            # ready to open video camera (and print info)
             camera = cv2.VideoCapture(videoCaptureDeviceId)
             ret = camera.read()[0]
             if ret:
@@ -158,54 +227,38 @@ async def main(argv):
 
             next_frame = 0 # limit to ~10 fps here
 
+            # run classifier (which also capture image)
             for res, img in runner.classifier(videoCaptureDeviceId):
                 if (next_frame > now()):
                     time.sleep((next_frame - now()) / 1000)
 
-                # print('classification runner response', res)
-
-                if "classification" in res["result"].keys():
-                    print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
-                    for label in labels:
-                        score = res['result']['classification'][label]
-                        print('%s: %.2f\t' % (label, score), end='')
-                    print('', flush=True)
-
-                    if (show_camera):
-                        cv2.imshow('edgeimpulse', img)
-                        if cv2.waitKey(1) == ord('q'):
-                            break
-
-                elif "bounding_boxes" in res["result"].keys():
-                    print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
-                    for bb in res["result"]["bounding_boxes"]:
-                        response = '\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height'])
-                        print(response)
-
-                        # TODO: create JSON and send telemetry as separate function
-                        # a Python object (dict):
-                        x = {
-                            "class": bb['label'],
-                            "score": bb['value'],
-                            "rect": {
-                                "x": bb['x'],
-                                "y": bb['y'],
-                                "width": bb['width'],
-                                "height": bb['height']
-                            }
-                        }
-
-                        # convert into JSON:
-                        y = json.dumps(x)
-                        await module_client.send_message_to_output(y, "classification")
+                print('classification runner response', res)
+                handled_result = handle_inference_result(res, img, labels)
+                await send_json_telemetry(module_client, handled_result)
 
                 next_frame = now() + 100
         finally:
             if (runner):
                 runner.stop()
 
+
+async def twin_patch_listener(module_client):
+        global TWIN_CALLBACKS
+        global SCORE_THRESHOLD
+        print("in twin_patch_listener")
+        while True:
+            try:
+                data = await module_client.receive_twin_desired_properties_patch()  # blocking call
+                print( "The data in the desired properties patch was: %s" % data)
+                if "ScoreThreshold" in data:
+                    SCORE_THRESHOLD = data["ScoreThreshold"]
+                    print("ScoreThreshold", SCORE_THRESHOLD)
+                TWIN_CALLBACKS += 1
+                print ( "Total calls confirmed: %d\n" % TWIN_CALLBACKS )
+            except Exception as ex:
+                print ( "Unexpected error in twin_patch_listener: %s" % ex )
+
 if __name__ == "__main__":
-   #main(sys.argv[1:])
 
    loop = asyncio.get_event_loop()
    loop.run_until_complete(main(sys.argv[1:]))
